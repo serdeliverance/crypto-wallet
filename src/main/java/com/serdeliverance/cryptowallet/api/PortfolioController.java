d6d3285be0c58d84b6573f132c0a09706c8c10b8
Sergio Cano <canosergio90@gmail.com>
2021-02-13 11:32:54 -0300

    transaction repository and modifications in dummy data

--- src/main/java/com/serdeliverance/cryptowallet/api/PortfolioController.java
+++ src/main/java/com/serdeliverance/cryptowallet/api/PortfolioController.java
@@ -2,6 +2,7 @@ package com.serdeliverance.cryptowallet.api;
 
 import com.serdeliverance.cryptowallet.dto.PorfolioDTO;
 import com.serdeliverance.cryptowallet.services.PortfolioService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/portfolios")
 @Slf4j
+@AllArgsConstructor
 public class PortfolioController {
 
     private PortfolioService portfolioService;
